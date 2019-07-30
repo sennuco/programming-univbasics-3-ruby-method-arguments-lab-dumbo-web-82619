c42184fadd5a7d168f80390122a8ae6b89fc1d81
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-30 01:34:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,8 @@ def introduction_with_language(name, language)
   puts ("Hi, my name is #{name} and I am learning to program in #{language}.\n")
 end
 
-language = "Ruby"
+
 def introduction_with_language_optional(name,language )
+  language = "Ruby"
   puts ("Hi, my name is #{name} and I am learning to program in #{language}.\n")
 end
